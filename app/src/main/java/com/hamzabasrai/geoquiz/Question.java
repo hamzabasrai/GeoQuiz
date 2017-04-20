51d23bb1cd848970cd8fea2c73a662d7f6fd410b
Hamza Basrai <hamza.basrai@gmail.com>
2017-04-19 23:10:03 -0400

    Initial commit

---
+++ app/src/main/java/com/hamzabasrai/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package com.hamzabasrai.geoquiz;
+
+/**
+ * Created by Hamza Basrai on 4/19/2017.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
