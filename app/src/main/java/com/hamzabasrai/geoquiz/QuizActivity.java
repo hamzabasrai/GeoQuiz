180f2745de4bc056c912bd9fb32cba9e0ad23481
Hamza Basrai <hamza.basrai@gmail.com>
2017-04-19 23:29:38 -0400

    Added previous button

--- app/src/main/java/com/hamzabasrai/geoquiz/QuizActivity.java
+++ app/src/main/java/com/hamzabasrai/geoquiz/QuizActivity.java
@@ -49,6 +49,7 @@ public class QuizActivity extends AppCompatActivity {
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
                 updateQuestion();
             }
         });
